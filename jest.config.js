eccca4a40a7c4c24656addd20edaa19720e91def
skywalker512 <houzhenhong@outlook.com>
2021-08-14 12:58:49 +0800

    chore: nx update

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
-  projects: [
-    '<rootDir>/libs/nx-vite',
-    '<rootDir>/apps/nx-vite-e2e',
-    '<rootDir>/libs/application',
-    '<rootDir>/apps/application-e2e',
-    '<rootDir>/libs/esbuild-plugin/transform',
-    '<rootDir>/libs/esbuild-plugin/source-map',
-    '<rootDir>/libs/esbuild-plugin/swc',
-  ],
+  projects: getJestProjects(),
 };
